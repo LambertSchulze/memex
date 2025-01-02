d08ccfb397aeaca124bca00ecefd2ea857e18a4e
LambertSchulze <schulze.lambert@googlemail.com>
2025-01-02 16:36:37 +0100

    Parse wikilinks in markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import wikiLinkPlugin from 'remark-wiki-link';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    markdown: {
+        remarkPlugins: [
+            [
+              wikiLinkPlugin,
+              {
+                permalinks: ["./"],
+                  aliasDivider: "|",
+                hrefTemplate: (link) => `./${link}`,
+                wikiLinkClassName: " ",
+                newClassName: " ",
+              },
+            ],
+        ]
+    }
+});
