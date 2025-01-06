792897eb24e215a4acc21381ab15bae1c3079136
LambertSchulze <schulze.lambert@googlemail.com>
2025-01-06 17:11:34 +0100

    Ignore markdown files from prettier

--- src/plugins/wikiLinkImages.mjs
+++ src/plugins/wikiLinkImages.mjs
@@ -2,16 +2,15 @@ import { findAndReplace } from "mdast-util-find-and-replace";
 import { u } from "unist-builder";
 
 export default function wikiLinkImages() {
-    
-    return function (tree) {
-        findAndReplace(tree, [
-            /!\[\[(.*)\]\]/g,
+  return function (tree) {
+    findAndReplace(tree, [
+      /!\[\[(.*)\]\]/g,
 
-            function (_, $1) {
-                const [path, alt] = $1.split("|");
+      function (_, $1) {
+        const [path, alt] = $1.split("|");
 
-                return u('image', { url: "@assets/" + path, alt: path ?? alt });
-            }
-        ])
-    }
+        return u("image", { url: "@assets/" + path, alt: path ?? alt });
+      },
+    ]);
+  };
 }
